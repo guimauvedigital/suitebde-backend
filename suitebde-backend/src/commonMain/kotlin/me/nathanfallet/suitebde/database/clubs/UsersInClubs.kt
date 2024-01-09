fe97c223132720d0924679bc0d002d91a223e091
NathanFallet <contact@nathanfallet.me>
2024-01-09 09:02:04 +0100

    Club models

---
+++ suitebde-backend/src/commonMain/kotlin/me/nathanfallet/suitebde/database/clubs/UsersInClubs.kt
@@ -0,0 +1,27 @@
+package me.nathanfallet.suitebde.database.clubs
+
+import me.nathanfallet.suitebde.models.clubs.Club
+import me.nathanfallet.suitebde.models.clubs.UserInClub
+import me.nathanfallet.suitebde.models.users.User
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
+
+object UsersInClubs : Table() {
+
+    val clubId = varchar("club_id", 32).index()
+    val userId = varchar("user_id", 32).index()
+
+    override val primaryKey = PrimaryKey(arrayOf(clubId, userId))
+
+    fun toUserInClub(
+        row: ResultRow,
+        club: Club? = null,
+        user: User? = null,
+    ) = UserInClub(
+        row[clubId],
+        row[userId],
+        club,
+        user
+    )
+
+}
