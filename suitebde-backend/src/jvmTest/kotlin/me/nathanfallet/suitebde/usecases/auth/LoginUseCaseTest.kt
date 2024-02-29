b27a0fd37bb9ca596c6e9fae7f8f904f0550021b
NathanFallet <contact@nathanfallet.me>
2024-02-29 14:55:54 +0100

    feat: fulfill subscription on buy

--- suitebde-backend/src/jvmTest/kotlin/me/nathanfallet/suitebde/usecases/auth/LoginUseCaseTest.kt
+++ suitebde-backend/src/jvmTest/kotlin/me/nathanfallet/suitebde/usecases/auth/LoginUseCaseTest.kt
@@ -6,7 +6,7 @@ import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import me.nathanfallet.suitebde.models.auth.LoginPayload
 import me.nathanfallet.suitebde.models.users.User
-import me.nathanfallet.suitebde.repositories.users.IUsersRepository
+import me.nathanfallet.suitebde.usecases.users.IGetUserForEmailUseCase
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -14,31 +14,31 @@ class LoginUseCaseTest {
 
     @Test
     fun invoke() = runBlocking {
-        val repository = mockk<IUsersRepository>()
+        val getUserForEmail = mockk<IGetUserForEmailUseCase>()
         val verifyPasswordUseCase = mockk<IVerifyPasswordUseCase>()
-        val useCase = LoginUseCase(repository, verifyPasswordUseCase)
+        val useCase = LoginUseCase(getUserForEmail, verifyPasswordUseCase)
         val user = User("id", "association", "email", "hash", "first", "last", false)
-        coEvery { repository.getForEmail("email", true) } returns user
+        coEvery { getUserForEmail("email", true) } returns user
         every { verifyPasswordUseCase("password", "hash") } returns true
         assertEquals(user, useCase(LoginPayload("email", "password")))
     }
 
     @Test
     fun invokeNoUser() = runBlocking {
-        val repository = mockk<IUsersRepository>()
+        val getUserForEmail = mockk<IGetUserForEmailUseCase>()
         val verifyPasswordUseCase = mockk<IVerifyPasswordUseCase>()
-        val useCase = LoginUseCase(repository, verifyPasswordUseCase)
-        coEvery { repository.getForEmail("email", true) } returns null
+        val useCase = LoginUseCase(getUserForEmail, verifyPasswordUseCase)
+        coEvery { getUserForEmail("email", true) } returns null
         assertEquals(null, useCase(LoginPayload("email", "password")))
     }
 
     @Test
     fun invokeBadPassword() = runBlocking {
-        val repository = mockk<IUsersRepository>()
+        val getUserForEmail = mockk<IGetUserForEmailUseCase>()
         val verifyPasswordUseCase = mockk<IVerifyPasswordUseCase>()
-        val useCase = LoginUseCase(repository, verifyPasswordUseCase)
+        val useCase = LoginUseCase(getUserForEmail, verifyPasswordUseCase)
         val user = User("id", "association", "email", "hash", "first", "last", false)
-        coEvery { repository.getForEmail("email", true) } returns user
+        coEvery { getUserForEmail("email", true) } returns user
         every { verifyPasswordUseCase("password", "hash") } returns false
         assertEquals(null, useCase(LoginPayload("email", "password")))
     }
